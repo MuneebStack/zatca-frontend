0640418a1a6c54b25e8b49f8ca621d24b1dc736d
MuneebStack <muneeb.creatives@gmail.com>
2025-11-08 02:51:15 +0500

    Add widget Card Sidebar

---
+++ src/pages/Dashboard/components/WidgetSidebar.tsx
@@ -0,0 +1,42 @@
+import type { WidgetType } from "@/types/widget";
+import { Drawer, List, Checkbox } from "antd";
+
+interface Props {
+    openDrawer: boolean;
+    onClose: () => void;
+    widgets: WidgetType[];
+    selected: WidgetType["name"][];
+    onToggle: (name: string, checked: boolean) => void;
+}
+
+export const WidgetSidebar: React.FC<Props> = ({
+    openDrawer,
+    onClose,
+    widgets,
+    selected,
+    onToggle
+}) => {
+    return (
+        <Drawer
+            title="Manage Widgets"
+            placement="right"
+            onClose={onClose}
+            open={openDrawer}
+        >
+            <List
+                dataSource={widgets}
+                renderItem={(item) => (
+                    <List.Item>
+                        <Checkbox
+                            checked={selected.includes(item.name)}
+                            onChange={(e) => onToggle(item.name, e.target.checked)}
+                        >
+                            {item.title}
+                        </Checkbox>
+                    </List.Item>
+                )}
+                bordered
+            />
+        </Drawer>
+    );
+};
