6b96474f00df2c4cd7d53e901bd530a3b697d4e8
MuneebStack <muneeb.creatives@gmail.com>
2025-11-13 06:57:18 +0500

    Minor improvements

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -2,7 +2,7 @@ export interface NavigationType {
     id: number;
     name: string;
     icon?: string;
-    route: string;
+    route?: string;
     parent_id?: number;
     order: number;
     created_at?: string;
